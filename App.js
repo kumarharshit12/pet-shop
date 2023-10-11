3bf5fc52e2294489df295bb5fd620b82f98e5747
kumar Harshit <88801031+kumarharshit12@users.noreply.github.com>
2023-10-11 11:24:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
+import ServiceList from './components/Dashboard/ServiceList';
+import UserProfile from './components/userProfile';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/dashboard/services" element ={<ServiceList/>} />
+        <Route path="/profile" element={<UserProfile/>} />
+      <Route element={<NotFound/>}/>
+      </Routes>
+    
+    </Router>
+  );
+}
+function NotFound(){
+  return <h1>404 - Not Found</h1>;
+}
+export default App;
